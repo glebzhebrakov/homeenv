773916ea2885f10501b35343e56eba0cfbf01f7a
Gleb Zhebrakov <gzhebrakov@sciforce.solutions>
2017-01-02 22:12:59 +0200

    Initial commit

---
+++ src/main/java/com/homeenv/HomeenvApplication.java
@@ -0,0 +1,12 @@
+package com.homeenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeenvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeenvApplication.class, args);
+	}
+}
