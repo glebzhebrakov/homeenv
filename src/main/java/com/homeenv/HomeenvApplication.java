4c445b4932d189c406115fbf50503e526cc17758
Gleb Zhebrakov <gzhebrakov@sciforce.solutions>
2017-02-06 17:39:45 +0200

    eureka

--- src/main/java/com/homeenv/HomeenvApplication.java
+++ src/main/java/com/homeenv/HomeenvApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableConfigurationProperties({ApplicationProperties.class})
 public class HomeenvApplication {
 
 	public static void main(String[] args) {
+//        System.setProperty("spring.config.name", "homeenv");
 		SpringApplication.run(HomeenvApplication.class, args);
 	}
 }
